eb50d9181a97ef98a0f96967083942d069374e1a
GriFFen75 <savadoux@et.esiea.fr>
2023-05-18 18:46:49 +0200

    captcha fonctionne + fonction de CSRF token

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 $mysqli = NULL;
 
 function join_database() {
-    #global $mysqli;
+    global $mysqli;
 
     $dbinfo = file_get_contents("join_db.json");
     $dbinfo = json_decode($dbinfo, true);
@@ -46,6 +46,29 @@ function join_database_secure(){
      return $db;
 }
 
+function join_database_secure_PDO(){
+
+    $dbinfo = file_get_contents("join_db.json");
+    $dbinfo = json_decode($dbinfo, true);
+
+        $options = [
+        PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/Prog_Web_Secu/key/Griffen.crt',
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true
+    ];
+
+    $dsn = 'mysql:host=pws.local;port=3306;dbname=prog_web_secu';
+    $usernameDB = $dbinfo["login"];
+    $passwordDB = $dbinfo["password"];
+
+    try {
+        $bdd = new PDO($dsn, $usernameDB, $passwordDB, $options);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connexion réussie en utilisant SSL.";
+    } catch (PDOException $e) {
+        echo "Erreur de connexion : " . $e->getMessage();
+    }
+}
+
 function insert_fields($table, $fields) {
     global $mysqli;
     $tab = array_keys($fields); //ici fields doit etre égale à un tableau ( dans notre cas : le message , l'id et quand le message à été posté)
@@ -78,5 +101,20 @@ function insert_field_secure($username, $password){
     }
 }
 
+
+function generateCSRFToken() {
+    $token = bin2hex(random_bytes(32)); // Génère une chaîne aléatoire de 32 octets (256 bits)
+    $_SESSION['csrf_token'] = $token; // Stocke le jeton CSRF dans la session
+    return $token;
+}
+
+// Vérification du jeton CSRF
+function validateCSRFToken($token) {
+    if (isset($_SESSION['csrf_token']) && $_SESSION['csrf_token'] === $token) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 ?>
--- inscription.php
+++ inscription.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <title>Prog_Web_Secu</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 
 <body>
@@ -14,6 +15,8 @@
         <input type="text" name="username" id="username" placeholder="username" minlength="5" maxlength="512" required><br>
         <input type="password" name="password" id="password" placeholder="password" minlength="4" maxlength="512" required><br>
         <input type="submit" name="submit" id="submit" value="inscription">
+        <div class="g-recaptcha" data-sitekey="6LeWZB0mAAAAAM6y6mE8q0sDFfpRkMISvwoHu-_m"></div>
+
     </form>
 
 </body>
@@ -22,7 +25,8 @@
 global $mysqli;
 include("functions.php");
 join_database();
-
+#join_database_secure();
+#join_database_secure_PDO();
 
 //$result = $mysqli->query("SELECT * FROM `user`");
 //$users = $result->fetch_assoc();
@@ -30,32 +34,60 @@ join_database();
 //echo "<br>";
 
 
+
+
 if (isset($_POST['username'])){
     if ($_POST['username'] != "") {
         if (isset($_POST["password"])) {
             if ($_POST["password"] != "") {
                 if (isset($_POST["submit"])) {
-                    $password_str = mysqli_real_escape_string($mysqli, $_POST['password']);
-                    if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{4,512}$/', $password_str)){
-                        $username_html = htmlspecialchars($_POST["username"]);
-                        $username_str = mysqli_real_escape_string($mysqli, $_POST['username']);
-                        $doublon = $mysqli->query("SELECT * FROM user WHERE username = '{$username_str}'")->fetch_assoc();
-                        if ($doublon){
-                            echo "Ce nom d'utilisateur est deja dans la base de donnée";
+
+
+                    $recaptcha_response = $_POST['g-recaptcha-response'];
+                    $secret_key = '6LeWZB0mAAAAAHZU1nQKTJD8jXe3qWUL9D8bbtSm';
+
+                    $url = 'https://www.google.com/recaptcha/api/siteverify';
+                    $data = array(
+                        'secret' => $secret_key,
+                        'response' => $recaptcha_response
+                    );
+
+                    $options = array(
+                        'http' => array(
+                            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                            'method' => 'POST',
+                            'content' => http_build_query($data)
+                        )
+                    );
+
+                    $context = stream_context_create($options);
+                    $result = file_get_contents($url, false, $context);
+                    $response = json_decode($result, true);
+
+
+                    if ($response['success']) {
+                        $password_str = mysqli_real_escape_string($mysqli, $_POST['password']);
+                        if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{4,512}$/', $password_str)){
+                            $username_html = htmlspecialchars($_POST["username"]);
+                            $username_str = mysqli_real_escape_string($mysqli, $_POST['username']);
+                            $doublon = $mysqli->query("SELECT * FROM user WHERE username = '{$username_str}'")->fetch_assoc();
+                            if ($doublon){
+                                echo "Ce nom d'utilisateur est deja dans la base de donnée";
+                            }
+                            else{
+                                insert_field_secure($username_str, password_hash($password_str, PASSWORD_BCRYPT, ['cost' => 10, 'salt' => "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDMKqUrabOE26b0oLF1gP1GGUCYv+H6GZnHEATOoRTMwFHmhTxVIhWd2p+YMkMNe49f/+kQRdniMY/mnEXEsAKkolF/o2F25o0SOjmOK1AxjsvPuYGExuWrpGmC0IKW3PDU26PodgFxcBM3qNB4iKrRyHc5ENYszlH7YMFeMXnpIWrczpvPRGOtitn8BNbHKwcKOUmZYoK7EzT61o96Cev395aW5AaGoijQO6qGIsXjzAfslC21CTa3DXc/EDg+o5b+wMRcSBTUovhWrwn9aluGWp5hx0lUUMuU9I7OiR9AhO6UYCIUnCgNTXQfK3gjU0EMQJdtYr9arNi4l9UrKupJAgMBAAECggEAJl4tRAdNMT6GFZrNPqPK9Q1x0kLdGL8O4xXjkWE8I25Q9d08BVOudEfNjjCD4VjDDOtuxRwbYiKmRFRB2ECrfnzyi+YMPuf8wtwmTM1e+LE5"] ));
+                                ?>
+                                <h1>Bonjour <?php echo $username_html; ?></h1>
+                                <?php
+                            }
                         }
                         else{
-                            insert_field_secure($username_str, password_hash($password_str, PASSWORD_BCRYPT, ['cost' => 10, 'salt' => "nEXEsAKkolF/o2F25o0SOjmOK1AxjsvPuYGExuWrpGmC0IKW3PDU26PodgFxcBM3"] ));
-                            ?>
-                            <h1>Bonjour <?php echo $username_html; ?></h1>
-                            <?php
+                            echo "Le mot de passe n'est pas assez fort";
                         }
                     }
                     else{
-                        print_r("Non non non");
+                        print_r("probleme avec le katchan");
                     }
-                }
-                else{
-                    echo "Le mot de passe n'est pas assez fort";
                 }
             }
         }
